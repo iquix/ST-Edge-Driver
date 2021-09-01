be08306c86dedad8cc53291afb7dd173ae79a97d
iquix <49198015+iquix@users.noreply.github.com>
2021-09-01 14:58:57 +0900

    Added preferences for temperature offset and motion sensitivity

--- hue-motion-sensor/src/init.lua
+++ hue-motion-sensor/src/init.lua
@@ -1,4 +1,4 @@
--- Hue Motion Sensor ver 0.1.0
+-- Hue Motion Sensor ver 0.2.0
 -- Copyright 2021 Jaewon Park (iquix)
 --
 -- Licensed under the Apache License, Version 2.0 (the "License");
@@ -17,6 +17,7 @@ local capabilities = require "st.capabilities"
 local ZigbeeDriver = require "st.zigbee"
 local defaults = require "st.zigbee.defaults"
 local clusters = require "st.zigbee.zcl.clusters"
+local cluster_base = require "st.zigbee.cluster_base"
 local data_types = require "st.zigbee.data_types"
 
 
@@ -29,6 +30,14 @@ local function illuminance_attr_handler(driver, device, value, zb_rx)
 	-- illuminance handler is explicitly defined because of the error in the default built-in zigbee handler.
 	local lux_value = math.floor(10 ^ ((value.value - 1) / 10000))
 	device:emit_event(capabilities.illuminanceMeasurement.illuminance(lux_value))
+end
+
+local function device_info_changed(driver, device, event, args)
+	if args.old_st_store.preferences.motionSensitivity ~= device.preferences.motionSensitivity then
+		local sensitivityTable = {sensitivityLow = 0, sensitivityMedium = 1, sensitivityHigh = 2}
+		device:send(cluster_base.write_manufacturer_specific_attribute(device, clusters.OccupancySensing.ID, 0x0030, 0x100b,
+			data_types.Uint8, sensitivityTable[device.preferences.motionSensitivity]))
+	end
 end
 
 local hue_motion_driver = {
@@ -68,6 +77,9 @@ local hue_motion_driver = {
 				reportable_change = 1000
 			}
 		}
+	},
+	lifecycle_handlers = {
+		infoChanged = device_info_changed
 	}
 }
 
